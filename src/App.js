76b85c121f748a69dbec2c05f470b89b98bd229a
yekta <yektabesharaty@gmail.com>
2024-08-25 16:22:46 +0330

    add project

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function AccordionItems({ num, title, text }) {
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleClick}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}-</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
 
       {isOpen && <div className="content-box">{text}</div>}
     </div>
